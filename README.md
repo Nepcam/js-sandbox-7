6d5c786ed6244b3337a133612abd09c31ad27a93
Nepcam <cameron.nepe@gmail.com>
2019-07-13 18:33:37 +1200

    working with Promises

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Callback functions
 
 10/07/2019
 - Callback function exercise
+
+13/07/2019
+- ES6 Promises
--- app.js
+++ app.js
@@ -3,9 +3,42 @@ const posts = [
   {title: 'Post Two', body: 'This is post two'},
 ];
 
-// function createPost(post) {
+function createPost(post) {
+ return new Promise(function(resolve, reject){
+   setTimeout(function() {
+     posts.push(post);
+
+     const error = false;
+
+     if(!error) {
+       resolve();
+     } else {
+       reject('Error: Something went wrong');
+     }
+   }, 2000);
+ });
+}
+
+function getPosts() {
+  setTimeout(function() {
+    let output = '';
+    posts.forEach(function(post) {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000)
+}
+
+createPost({title: 'Post Three', body: 'This is post three'}).then(getPosts).catch(function(err) {
+  console.log(err);
+});
+
+// getPosts();
+
+// function createPost(post, callback) {
 //   setTimeout(function() {
 //     posts.push(post);
+//     callback();
 //   }, 2000);
 // }
 
@@ -19,25 +52,4 @@ const posts = [
 //   }, 1000)
 // }
 
-// createPost({title: 'Post Three', body: 'This is post three'});
-
-// getPosts();
-
-function createPost(post, callback) {
-  setTimeout(function() {
-    posts.push(post);
-    callback();
-  }, 2000);
-}
-
-function getPosts() {
-  setTimeout(function() {
-    let output = '';
-    posts.forEach(function(post) {
-      output += `<li>${post.title}</li>`;
-    });
-    document.body.innerHTML = output;
-  }, 1000)
-}
-
-createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
+// createPost({title: 'Post Three', body: 'This is post three'}, getPosts);
